8d203b2b99fbe1d0c36411b2d488c4391c8cc598
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-11-09 00:32:39 +0600

    setup database from mongodb, apply get and post

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 const express = require("express");
-const packageName = require('packageName');
+const cors = require("cors");
+require("dotenv").config();
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yh8mwoi.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("Community-cleanliness");
+    const issuesCollection = database.collection("issues");
+
+    app.get("/issues", async (req, res) => {
+      const cursor = issuesCollection.find();
+      const issues = await cursor.toArray();
+      res.send(issues);
+    });
+
+    app.post("/issues", async (req, res) => {
+      const issue = req.body;
+      const results = await issuesCollection.insertOne(issue);
+      res.send(results);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("port is running at", port);
+});
