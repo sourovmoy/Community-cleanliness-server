395eacb7a1bcebf89e2a8b7b765a52e0f095c014
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-11-11 11:06:27 +0600

    remove all the useless text

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
